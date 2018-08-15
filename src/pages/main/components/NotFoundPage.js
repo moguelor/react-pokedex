9fd95169e2ef5c16b8f8491af3f4b3dbee51a3f7
Jose Obed Moguel Ruiz <jo_obed@hotmail.com>
2018-08-14 20:19:58 -0500

    Add layout to main page.
    Add layout to main page for show in specified route "index", configuring reducer, container and components.

---
+++ src/pages/main/components/NotFoundPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NotFoundPage = () => (
+    <div> Sorry, page not found. </div>
+)
+
+export default NotFoundPage
---
+++ src/pages/main/components/index.js
@@ -0,0 +1,9 @@
+import App from './App'
+import NotFoundPage from './NotFoundPage'
+import HomePage from './HomePage'
+
+export {
+    App,
+    NotFoundPage,
+    HomePage
+}
